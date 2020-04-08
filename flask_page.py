6faa2da76f4f415f721042890a2718f5689f455d
Tim Shine <timshine124@gmail.com>
2020-04-08 17:47:55 -0400

    completed flask page - still need to fix button look

--- flask_page.py
+++ flask_page.py
@@ -4,22 +4,35 @@ video. It will also offer button for user control"""
 # Import necessary packages to open flask page
 import os
 from HOG_NMS import read_video_stream
-from flask import Response, Flask, render_template
+from flask import Response, Flask, render_template, request, url_for, flash
 from threading import Thread
 import argparse
 import datetime
 import imutils
 import time
 import cv2
+import sys_information
 
 # Begin flask object
 app = Flask(__name__)
 
 
-@app.route("/live_stream_video.html")               
-def video():
+# @app.route("/live_stream_video.html")               
+# def video():
+#     # Return the template
+#     return render_template("live_stream_video.html")
+
+@app.route("/live_stream_video.html", methods=['POST', 'GET'])               
+def buttonClickOnVideo():
     # Return the template
+    if request.method == 'POST':
+        if request.form['videoButton'] == 'target':
+            output = "Mis-identified"
+        elif request.form['videoButton'] == 'home':
+            output = "Return Home"
+        print(output)
     return render_template("live_stream_video.html")
+
 
 @app.route("/about.html")               
 def about():
@@ -30,6 +43,29 @@ def about():
 def home():
     # Return the template
     return render_template("index.html")
+
+@app.route("/l3harris.html")               
+def l3harris():
+    # Return the template
+    return render_template("l3harris.html")
+
+@app.route("/sys_stats.html")               
+def sys_stats():
+    # Return the template
+    sys_info = sys_information.get_sys_info()
+    cpu_info = sys_information.get_cpu_cores()
+    boot_time = sys_information.get_boot_time()
+    memory_info = sys_information.get_memory_info()
+    return render_template("sys_stats.html", sys_info=sys_info, cpu_info=cpu_info, boot_time = boot_time, memory_info=memory_info)
+
+@app.route("/sys_stats.html", methods=['POST'])
+def update_stats():
+    #updating stats
+    sys_info = sys_information.get_sys_info()
+    cpu_info = sys_information.get_cpu_cores()
+    boot_time = sys_information.get_boot_time()
+    memory_info = sys_information.get_memory_info()
+    return render_template('sys_stats.html', sys_info=sys_info, cpu_info=cpu_info, boot_time=boot_time, memory_info=memory_info)
 
 @app.route("/")               
 def original():
@@ -49,4 +85,4 @@ if __name__=='__main__':
     # t1 = Thread(target = read_video_stream)
     # t1.start()
     # start flask app
-    app.run(host='0.0.0.0', debug=True, threaded=True)
+    app.run(host='0.0.0.0', debug=False, threaded=True)
